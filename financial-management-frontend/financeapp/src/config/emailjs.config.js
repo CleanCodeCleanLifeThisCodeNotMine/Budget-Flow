eab1e4f948bd351c0790c49949cf587d76a3c9ca
ImClean <ng.hoangson93@gmail.com>
2025-03-10 14:30:17 +0700

    fix

---
+++ financial-management-frontend/financeapp/src/config/emailjs.config.js
@@ -0,0 +1,7 @@
+// EmailJS configuration
+export const EMAILJS_CONFIG = {
+  publicKey: '6xS7gVcnrNq7WUmDe',
+  serviceId: 'service_fl181h7',
+  resetTemplateId: 'your_reset_template_id', // TODO: Update with actual reset template ID
+  activationTemplateId: 'template_2g5m1mb'
+}; 
